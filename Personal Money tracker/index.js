2168b6dd25be078e318bd09eaa74af68e063a8ab
unknown <hyeslee99@gmail.com>
2020-08-24 22:52:00 +0900

    Finished nav-bar. worked on the adding forms

---
+++ Personal Money tracker/index.js
@@ -0,0 +1,41 @@
+﻿// JavaScript source code
+(function () {
+    "use strict";
+
+    window.addEventListener("load", initialize);
+
+
+    function initialize() {
+        qs(".btn1").addEventListener("click", askForIncome);
+        qs(".btn2").addEventListener("click", askForSpending);
+    }
+
+    function askForIncome() {
+        var box = $("addi");
+        box.style.display = "block";
+        qs(".addbtn").addEventListener("click", addTheData);
+    }
+
+    // Add the income or spending 
+    function addTheData() {
+
+    }
+
+    // for test purpose! 
+    function myFunction() {
+        alert("Hi");
+    }
+
+    function askForSpending() {
+        var box = $("adds");
+        box.style.display = "block";
+    }
+
+    function $(id) {
+        return document.getElementById(id);
+    }
+
+    function qs(query) {
+        return document.querySelector(query);
+    }
+})();
