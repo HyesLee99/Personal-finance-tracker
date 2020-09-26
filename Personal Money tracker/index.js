4af2d49443b7ee0797da42be798e36c0209bb581
unknown <hyeslee99@gmail.com>
2020-09-26 18:50:46 +0900

    computing the currency and add to the chart

--- Personal Money tracker/index.js
+++ Personal Money tracker/index.js
@@ -7,21 +7,15 @@
     let CURRENCY = "USD";
     let BASE = "USD";
     let VALUE = 0;
+    var request = new XMLHttpRequest()
 
-
+    // Initialize the page and allow buttons to be functional 
     function initialize() {
         qs(".btn1").addEventListener("click", askForIncome);
         qs(".btn2").addEventListener("click", askForSpending);
     }
 
-    function checkStatus(response) {
-        if (response.status >= 200 && response.status < 300 || response.status == 0) {
-            return response.text();
-        } else {
-            return Promise.reject(new Error(response.status + ": " + response.statusText));
-        }
-    }
-
+    // Ask the income (title, amount, date)
     function askForIncome() {
         var box = $("addi");
         box.style.display = "block";
@@ -29,6 +23,7 @@
         qs(".add-close").addEventListener("click", closingIncome);
     }
 
+    // Ask the spending (title, amount, date)
     function askForSpending() {
         var box = $("adds");
         box.style.display = "block";
@@ -36,46 +31,65 @@
         qs(".spend-close").addEventListener("click", closingSpend);
     }
 
+    // closing button for income
     function closingIncome() {
         var box = $("addi");
         box.style.display = "none";
     }
 
+    // closing button for spending
     function closingSpend() {
         var box = $("adds");
         box.style.display = "none";
     }
 
+    // Using foreign exchange rates API, get the currency rate 
+    // and compute the currency to USD 
     function getCurrency() {
         let currencyDom = qs(".currency-select");
         CURRENCY = currencyDom.options[currencyDom.selectedIndex].value;
         let date = $("income-date").value;
         let urlValue = URL + "/" + date + "?base=" + BASE;
         console.log(urlValue);
-        fetch(urlValue, { mode: "no-cors" })
-            .then(checkStatus)
-            .then(JSON.parse)
-            .then(findCurrency)
-            .catch(console.log)
-    }
-
-    function findCurrency(currencyList) {
-        if (currencyList.length == 0) {
-            console.log("error");
-        } else {
-            let list = currencyList[0].rates;
-            for (let i = 0; i < list.length; i++) {
-                if (CURRENCY == list[i]) {
-                    let rate = list[i].CURRENCY;
-                    addIncome(rate);
-                }
+        request.open('GET', urlValue, true)
+        request.responseType = 'json';
+        request.send();
+        request.onload = function () {
+            let data = request.response;
+            if (request.status >= 200 && request.status < 400) {
+                if (data.rates.length == 0) {
+                    console.log("error");
+                } else {
+                    let flag = false;
+                    let currencyList = data.rates;
+                    for (let key in currencyList) {
+                        if (key == CURRENCY) {
+                            console.log("found the currency rate!");
+                            addIncome(currencyList[key]);
+                            flag = true; 
+                        }
+                    }
+                    if (!flag) {
+                        alert("currency not found! ");
+                    }
+                    
+                } 
             }
-            console.log("not found");
+            
         }
     }
 
+    function testing() {
+        console.log(currencyList[i] + "is not currency that I am looking for, " + CURRENCY);
+        if (CURRENCY == currencyList[i]) {
+            let rate = currencyList[i].CURRENCY;
+            addIncome(rate);
+            console.log("successfully added! " + rate);
+        }
+    }
 
-    // Add the income or spending 
+    // Add the income to the chart and convert the amount by the 
+    // "Base" currency of the date. 
     function addIncome(rate) {  
         let oneRow = document.createElement("tr");
         let dateBox = document.createElement("th");
@@ -86,14 +100,11 @@
         let date = $("income-date").value;
         let title = $("income-title").value;
         let amount = $("income-amount").value;
-        let currencyDom = qs(".currency-select");
-        let currency = currencyDom.options[currencyDom.selectedIndex].text;
         dateBox.innerText = date;
         titleBox.innerText = title;
-        amountBox.innerText = amount * rate;
+        amountBox.innerText = Math.round(amount / rate * 100) / 100;
         category.innerText = "Income";  
-        currencyBox.innerText = currency;
-        getCurrency(urlValue);
+        currencyBox.innerText = CURRENCY;
         oneRow.appendChild(dateBox);
         oneRow.appendChild(titleBox);
         oneRow.appendChild(amountBox);
@@ -102,8 +113,8 @@
         qs(".table-body").appendChild(oneRow);
     }
 
-    
-
+    // Add the spending to the chart and convert the amount by the 
+    // "Base" currency of the date.
     function addSpending() {
         let oneRow = document.createElement("tr");
         let dateBox = document.createElement("th");
@@ -134,7 +145,14 @@
         alert("Hi");
     }
 
-    
+    // check status of the returned data
+    function checkStatus(response) {
+        if (response.status >= 200 && response.status < 300 || response.status == 0) {
+            return response.text();
+        } else {
+            return Promise.reject(new Error(response.status + ": " + response.statusText));
+        }
+    }
 
     function $(id) {
         return document.getElementById(id);
